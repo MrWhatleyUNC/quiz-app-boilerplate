a8c75cad479ba6c6a5d415843e59b53ee04ed234
MrWhatleyUNC <chris.whatley87@gmail.com>
2020-08-13 09:54:33 -0400

    fully functional and styled

--- app.js
+++ app.js
@@ -14,15 +14,17 @@ const STORE = {
         'Austin',
       ],
       correctAnswer: 'Austin'
+      
     },
     {
-      question: 'What is the capital ofNorth Carolina?',
+      question: 'What is the capital of North Carolina?',
       answers: [
         'Fayetteville',
         'Charlotte',
         'Raleigh',
       ],
       correctAnswer: 'Raleigh'
+      
     },
     {
       question: 'What is the capitol of Georgia?',
@@ -31,7 +33,8 @@ const STORE = {
         'Atlanta',
         'Columbus',
       ],
-      correctAnswer: 'Atlanta'
+      correctAnswer: 'Atlanta',
+      
     },
     {
       question: 'What is the capitol of California?',
@@ -41,6 +44,7 @@ const STORE = {
         'Sacremento',
       ],
       correctAnswer: 'Sacremento'
+      
     },
     {
       question: 'What is the capitol of Wisconsin?',
@@ -50,12 +54,13 @@ const STORE = {
         'Milwaukee',
       ],
       correctAnswer: 'Madison'
+      
     },
   ],
-  quizStarted: false,
-  questionNumber: 0,
+  questionIndex: 0,
   score: 0
 };
+let score = STORE.score
 
 /**
  * 
@@ -75,82 +80,139 @@ const STORE = {
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
-
-
-function generateQuestionPage(){
-  //this should create the html for the question page
-      $('main').html(` 
-      <h1>Question ${STORE.questionNumber+1}</h1>
-        <div class="container">${STORE.questions[STORE.questionNumber].question}</div>
-        <h2>Answers:</h2>
-        <form action="submit" class="answers">
-          <input type="radio" id="ans1" name="answer" value="ans1">
-          <label for="ans1">${STORE.questions[STORE.questionNumber].answers[0]}</label><br>
-          <input type="radio" id="ans2" name="answer" value="ans2">
-          <label for="ans2">${STORE.questions[STORE.questionNumber].answers[1]}</label><br>
-          <input type="radio" id="ans3" name="answer" value="ans3">
-          <label for="ans3">${STORE.questions[STORE.questionNumber].answers[2]}</label><br>
-          <button type="submit">Submit</button>
-        </form>`
-  )};
-
-function generateStartPg(){
-  let start = [];
-  start.push(`
-  <div class="container">
+function generateStartPg() {
+  return $('main').html(`
+  <div class="container start">
     <button class='start'>Get Started</button>
   </div>`)
-  return start
 }
 
-// function generateQuestionsArray(){
-//   let html =[];
-//   for(let i=0; i<=STORE.questions.length; i++){
-//     let questions = STORE.questions[i]
-//     console.log(questions)
-//     console.log((i+1))
-//     html.push(`
-//     <h1>Question ${(i+1)}</h1>
-//     <div class="container">${questions['question']}</div>
-//     <h2>Answers:</h2>
-//     <form>
-//       <input type="radio" id="${questions.answers[0]}" name='question${(i+1)}' value="${questions.answers[0]}">
-//       <label for="${questions.answers[0]}">${questions.answers[0]}</label><br>
-//       <input type="radio" id="${questions.answers[1]}" name='question${(i+1)}' value="${questions.answers[1]}">
-//       <label for="${questions.answers[1]}">${questions.answers[1]}</label><br>
-//       <input type="radio" id="${questions.answers[2]}" name='question${(i+1)}' value="${questions.answers[2]}">
-//       <label for="${questions.answers[2]}">${questions.answers[2]}</label><br>
-//       <button type="submit">Submit</button>
-//     </form>
-//     `)
-//   }
-// }
+function generateQuestionPage() {
+  //this should create the html for the question page
+  return $('main').html(`
+  <div class="box"> 
+    <h2>Question ${STORE.questionIndex+1} of ${STORE.questions.length}</h2>
+    <div class="container">${STORE.questions[STORE.questionIndex].question}</div>
+    <h2>Answers:</h2>
+    <div class="container">
+    <form action="submit" class="answers">
+      <label for="ans1">
+      <input type="radio" id="ans1" name="answer" value="${STORE.questions[STORE.questionIndex].answers[0]}">
+      ${STORE.questions[STORE.questionIndex].answers[0]}
+      </label><br>
+      <label for="ans2">
+      <input type="radio" id="ans2" name="answer" value="${STORE.questions[STORE.questionIndex].answers[1]}">
+      ${STORE.questions[STORE.questionIndex].answers[1]}
+      </label><br>
+      <label for="ans3">
+      <input type="radio" id="ans3" name="answer" value="${STORE.questions[STORE.questionIndex].answers[2]}">
+      ${STORE.questions[STORE.questionIndex].answers[2]}
+      </label><br>
+      <button class='submit' type="submit">Submit</button>
+    </form>
+    <div class='results'></div>
+    <button class='next'>Next Question</button>
+    </div>
+  </div>
+  `)
+};
+
+function generateResultsHTML(answer) {
+  let correct = STORE.questions[STORE.questionIndex].correctAnswer
+  let results = []
+
+  if (answer === correct) {
+    STORE.score++
+    results.push(`
+    <div class="result">
+    Your answer is correct. Your current score is ${STORE.score} out of ${STORE.questions.length}.
+    </div>
+    `)
+    return results
+  } else {
+    results.push(`
+    <div class="result">
+    Your answer is incorrect. The correct answer was ${correct}. Your current score is ${STORE.score} out of ${STORE.questions.length}.
+    </div>
+    `)
+    return results
+  }
+}
+
+function generateFinalPg() {
+  return $('main').html(`
+  <div class="box">
+  <h1>Final Score</h1>
+  <div class="container">
+    <div class="box">${STORE.score} out of ${STORE.questions.length}</div>
+    <button class='again'>Play Again</button>
+  </div>
+  </div>
+  `)
+}
 
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
 
-function renderStartPg(){
-  let html = generateStartPg()
-  console.log(html)
-  $('main').html(html)
+function renderStartPg() {
+  generateStartPg()
 }
 
-function renderQuestions(){
-  console.log('questions rendered')
-  // let html = generateQuestionsArray()
-  // console.log(html)
-  // $('main').html(html)
+function renderQuestions() {
+  console.log(STORE.questionIndex)
+  if (STORE.questionIndex < STORE.questions.length) {
+    console.log(STORE.questionIndex)
+    generateQuestionPage()
+    console.log('questions rendered')
+  } else {
+    renderFinalPg()
+  }
+}
+
+function renderResults(results) {
+  console.log('results rendered')
+  $('main').find('div.results').html(results)
+}
+
+function renderFinalPg() {
+  console.log('final page rendered')
+  generateFinalPg()
+  console.log(STORE.score)
+  console.log(STORE.questionIndex)
 }
 /********** EVENT HANDLER FUNCTIONS **********/
-$('main').on('click','button.start',renderQuestions)
+$('main').on('click', 'button.start', renderQuestions)
+$('main').on('submit', submitAnswer)
+$('main').on('click', 'button.next', renderQuestions)
+$('main').on('click', 'button.again', startQuiz)
 // These functions handle events (submit, click, etc)
 
-function main(){
+function submitAnswer(e) {
+  e.preventDefault();
+  console.log('answer submitted')
+  let answer = $('input:checked').val();
+  console.log(answer)
+  if(answer !== undefined){
+    let results = generateResultsHTML(answer)
+    renderResults(results)
+    $('main').find('button.submit').remove()
+    STORE.questionIndex++
+    console.log(STORE.questionIndex)
+    }
+  else{
+    alert('Please answer the question before submission.')
+  }
+}
+
+function main() {
   startQuiz()
 }
 
-function startQuiz(){
+function startQuiz() {
+  STORE.score=0
+  STORE.questionIndex=0
+  console.log(STORE)
   generateStartPg()
   renderStartPg()
 }
